a69f9e8c275354bc9345b5f18c1beb56b7ad8359
Ian Devlin <ian@iandevlin.com>
2013-02-03 14:32:23 +0100

    Added formatting to datetime.

--- time-hover.js
+++ time-hover.js
@@ -9,7 +9,7 @@
 */
 var timeHover = function() {
 	
-	var apply = function() {
+	var addHover = function() {
 		var time = document.getElementsByTagName('time');
 		var len = time.length;
 		for (var i = 0; i < len; i++) {
@@ -19,7 +19,7 @@ var timeHover = function() {
 				if (datetime.length > 0) {
 					var span = document.createElement('span');
 					span.setAttribute('class', 'datetime');
-					span.innerHTML = datetime;
+					span.innerHTML = formatDateTime(datetime);					
 					var w = t.offsetWidth;
 					span.setAttribute('style', 'margin-left:-' + (w - 5) + 'px;width:' + w + 'px');
 					t.appendChild(span);				
@@ -32,7 +32,36 @@ var timeHover = function() {
 				}
 			}
 		}
-	}	
-	apply();
+	}
+	// Month names for display date
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	var formatDateTime = function(datetime) {
+		// The separator values for display date
+		var dateSep = ' ', timeSep = ':';
+	
+		// Allowed values are fairly strict which aids recognising what's in there
+		var dateSepCount = datetime.split('-').length - 1;
+		if (dateSepCount == 0) return datetime;
+		var hasMonth = dateSepCount > 0;
+		var hasDay = dateSepCount > 1;
+		
+		// Build date string
+		var s = '';
+		var d = new Date(datetime);
+		if (hasDay) s += d.getDate() + dateSep;
+		if (hasMonth) s += months[d.getMonth()] + dateSep;
+		s += d.getFullYear();
+		// Build time string
+		var hasTime = ((datetime.split(':').length - 1) > 0);
+		if (hasTime) s += ' ' + d.getHours() + timeSep + addZero(d.getMinutes()) + timeSep + addZero(d.getSeconds());
+		console.log(s);
+		return s;
+	}
+	var addZero = function(number) {
+		return ((parseInt(number) < 10) ? '0' : '') + number;
+	}
+	
+	// Go!
+	addHover();
 	
 }
