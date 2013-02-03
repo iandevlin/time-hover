69a24f7b30feccfdae37c4dd61740661598e60e9
Ian Devlin <ian@iandevlin.com>
2013-02-03 12:16:55 +0100

    Added check for is null around datetime.

--- time-hover.js
+++ time-hover.js
@@ -15,19 +15,21 @@ var timeHover = function() {
 		for (var i = 0; i < len; i++) {
 			var t = time[i];
 			var datetime = t.getAttribute('datetime');
-			if (datetime.length > 0) {
-				var span = document.createElement('span');
-				span.setAttribute('class', 'datetime');
-				span.innerHTML = datetime;
-				var w = t.offsetWidth;
-				span.setAttribute('style', 'margin-left:-' + (w - 5) + 'px;width:' + w + 'px');
-				t.appendChild(span);				
-				t.addEventListener('mouseover', function() {
-					this.querySelectorAll('.datetime')[0].style.display = 'inline';
-				});				
-				t.addEventListener('mouseout', function() {
-					this.querySelectorAll('.datetime')[0].style.display = 'none';
-				});
+			if (datetime !== null) {
+				if (datetime.length > 0) {
+					var span = document.createElement('span');
+					span.setAttribute('class', 'datetime');
+					span.innerHTML = datetime;
+					var w = t.offsetWidth;
+					span.setAttribute('style', 'margin-left:-' + (w - 5) + 'px;width:' + w + 'px');
+					t.appendChild(span);				
+					t.addEventListener('mouseover', function() {
+						this.querySelectorAll('.datetime')[0].style.display = 'inline';
+					});				
+					t.addEventListener('mouseout', function() {
+						this.querySelectorAll('.datetime')[0].style.display = 'none';
+					});
+				}
 			}
 		}
 	}	
